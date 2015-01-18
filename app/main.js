d1739c0bb77c47df3a0edbe8004f1b25e2d4fc0b
Kyle Shipley <shipstar@gmail.com>
2015-01-18 11:41:51 -0500

    Add a jQuery dependency.

--- app/main.js
+++ app/main.js
@@ -1,3 +1,10 @@
-require(['foo'], function (foo) {
-  foo()
+require.config({
+  paths: {
+    jquery: 'node_modules/jquery/dist/jquery'
+  }
+})
+
+require(['jquery', 'foo'], function ($, foo) {
+  $(document.body).html("<h1>Foo</h1>");
+  foo();
 })
